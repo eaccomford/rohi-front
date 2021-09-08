4bac47cc4e26779f9e0522494141e9d073978bb1
Emmanuel Accomford <eaccomford@gmail.com>
2021-09-08 07:49:13 +0000

    add site header

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,29 @@
 import Image from 'next/image'
+import { SearchIcon, GlobeAltIcon,MenuIcon,UserCircleIcon,UsersIcon } from '@heroicons/react/solid'
 const Header = () => {
     return (
-        <header className="sticky top-0 z-50 grid-cols-3 bg-white shadow-md">
+        <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-md md:px-10">
+            {/* logo */}
             <div className="relative flex items-center h-10 my-auto cursor-pointer"> 
-                <Image src="http://localhost:1337/uploads/small_4000_23712fe719.jpg?459520.10000002384" layout="fill" objectFit="contain" objectPosition="left"/>
+                <Image className="rounded-full" src="http://localhost:1337/uploads/small_4000_23712fe719.jpg?459520.10000002384" layout="fill" objectFit="contain" objectPosition="left"/>
             </div>
-            <div></div>
-            <div></div>
+            {/* search */}
+            <div className="flex items-center py-2 rounded-full md:border-2 md:shadow-sm">
+                <input type="text" placeholder="enter search terms" className="flex-grow pl-5 text-sm text-gray-400 bg-transparent outline-none"/>
+                <SearchIcon className="hidden h-8 p-2 text-white bg-red-400 rounded-full cursor-pointer md:inline-flex md:mx-2"/>
+            </div>
+            {/* navs */}
+            <div className="flex items-center justify-end space-x-5 text-gray-500">
+                <p className="hidden cursor-pointer md:inline">Become a host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer"/>
+                <div className="flex items-center p-2 space-x-2 border-2 rounded-full">
+                    <MenuIcon className="h-6" />
+                    <UserCircleIcon className="h-6"/>
+                </div>
+            </div>
         </header>
     )
 }
 
 export default Header
+ 
